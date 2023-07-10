8208cf1a9b0c7a3e3cb57a8dba9f7056e48150ac
TnTomato <474093103@qq.com>
2023-07-10 16:57:58 +0800

    test: async test support

--- tests/test_copilot.py
+++ tests/test_copilot.py
@@ -1,7 +1,11 @@
 import os
 
+import pytest
+
 from src.fastcopilot.copilot import Copilot
 
+
+pytest_plugins = ('pytest_asyncio',)
 
 api_key = os.getenv("OPENAI_API_KEY")
 
@@ -9,4 +13,11 @@ api_key = os.getenv("OPENAI_API_KEY")
 def test_copilot():
     copilot = Copilot(api_key=api_key)
     reply, _ = copilot.run("hi")
+    assert isinstance(reply, str)
+
+
+@pytest.mark.asyncio
+async def test_async_copilot():
+    copilot = Copilot(api_key=api_key)
+    reply, _ = await copilot.arun("hi")
     assert isinstance(reply, str)
